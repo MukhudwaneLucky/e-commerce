5590d2736bbf3d4794ac5f9893afbc8e14912271
MukhudwaneLucky <mukhudwane17@gmail.com>
2020-04-16 19:48:20 +0200

    initial commit

---
+++ ImpalaFarming/ImpalaFarming/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImpalaFarming.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
